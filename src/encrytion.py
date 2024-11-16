99f2f99d86b510e0a950d2daab04422dc652a658
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2024-11-16 20:17:42 +0200

    Changed encoding/decoding so to make conceptually simpler and better integradted with my approach

--- src/encrytion.py
+++ src/encrytion.py
@@ -144,7 +144,7 @@ def decode_chess_game(pgn, quiet=False):
 
 
 # Returns binary number
-def my_string_encryption(input_string):
+def my_string_to_bin(input_string):
     bit_packets = []
     for char in input_string:
         string_representation = str(bin(ord(char)))[2:]
@@ -160,7 +160,7 @@ def my_string_encryption(input_string):
 
 
 # input must a binary number (example: `0b0101001`)
-def my_string_decrypt(bits):
+def my_bin_to_string(bits):
 
     message = ""
     # start with least bytes
@@ -219,10 +219,69 @@ def decode_games(dir_path):
         # print(bits)
     print(my_string_decrypt(running_bits))
 
-def main():
 
-    # TODO: load in messages from `data/messages/message1`
+
+# Converts a string message into binary numbers as a string type
+def secret_to_bin(secret_mssg):
+    bin_representation = ""
+    for char in secret_mssg:
+        # ASCII conversion always has leading dead bit of 0, python trims this off
+        #   so I am fixing the format here to ensure that the 0 shows in the string
+        # Note the indexing of temp_repr to skim off the "0b" at the start
+        temp_repr = str(bin(ord(char)))[2:]
+        
+        # Pre-pad string with needed 0s to ensure 8 bit-width (Also note dead but is 1)
+        needed_0s = 8 - len(temp_repr)
+        proper_repr = "1"
+        for _ in range(needed_0s - 1):
+            proper_repr += "0"
+        proper_repr += temp_repr
+        bin_representation += proper_repr
+
+    return bin_representation
+
+
+# Converts binary number of string type and converts it to out secret message text
+def bin_to_secret(bin_secret):
+
+    running_secret = ""
+    # Break up into 8 bit packets and convert using ascii
+    num_chars = int(len(bin_secret) / 8)
+    for _ in range(num_chars):
+        # Pop first byte and leave remaining bytes
+        popped_byte = bin_secret[:8]
+        bin_secret =  bin_secret[8:]
+        
+        # Change first bit back to `0` and change to binary number
+        popped_byte = "0" + popped_byte[1:]
+        decoded_char = chr(int(popped_byte, 2))
+        running_secret += decoded_char
+    return running_secret
+
+
+def encode_secret(path_to_secret):
     
+    with open(path_to_secret, "r") as tf:
+        secret_string = tf.read()
+    
+
+    bin_secret = secret_to_bin(secret_string)   
+
+
+    # TODO take out this functionallity, using this to ensure things work
+    with open("src/data/message1/secret-bin-repr.txt", "w") as tf:
+        tf.write(bin_secret)
+
+
+def main():
+    
+    # encode_secret("src/data/message1/secret.txt")
+
+    with open("src/data/message1/secret-bin-repr.txt", "r") as tf:
+        bin_secret = tf.read()
+    myString = bin_to_secret(bin_secret)
+    print(myString)
+    return 
     # games = encode_chess_game(binary_message)
     # save_game_for_bots(games)
 
@@ -232,9 +291,9 @@ def main():
     return
     with open("src/data/PlayedGames/game1.pgn", "r") as tf:
         pgn = tf.read()
-    bits = decode_chess_game(pgn)
-    print(my_string_decrypt(bits))
-    return
+        bits = decode_chess_game(pgn)
+        print(my_string_decrypt(bits))
+        return
 
 
 if __name__ == "__main__":
